ee48af9a5fd3b924daa837e9358d876c64cc377a
Rafael Silva <rasilva.1986@gmail.com>
2020-06-05 05:18:21 +0200

    first commit

---
+++ backend-tests/cypress/integration/backend-auth.spec.js
@@ -0,0 +1,24 @@
+
+describe('testing auth', function(){
+    it ('test case 1', function(){
+        cy.authenticateSession().then((response =>{
+            cy.request({
+                method: "GET",
+                url: 'http://localhost:3000/api/clients',
+                headers:{
+                    'X-User-Auth': JSON.stringify(Cypress.env().loginToken),
+                    'Content-Type': 'application/json'
+                },
+            }).then((response =>{
+                cy.log(response.body[0].id)
+                cy.log(response.body[0].created)
+                cy.log(response.body[0].name)
+                cy.log(response.body[0].email)
+                cy.log(response.body[0].telephone)
+
+            }))
+        }))
+    })
+
+
+})
